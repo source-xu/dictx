a47aae4ee509bf37f5798c48579688ae77b93c99
87602307 <56073532+source-xu@users.noreply.github.com>
2023-06-28 10:02:34 +0800

    Update dictx.py

--- dictx.py
+++ dictx.py
@@ -1,5 +1,6 @@
-﻿import base64
 import os
+
+from pinyintokenizer import PinyinTokenizer
 
 banner = """
                                                     
@@ -9,47 +10,69 @@ banner = """
   >  <| |_| |      | |_) | (_| \__ \__ \ |_|   |_|  
  /_/\_\\__,_|      | .__/ \__,_|___/___/            
                    | |                              
-                   |_|                   V-2.1 by xu       
+                   |_|                   V-3.0 by xu       
          """
 print(banner)
 
 # 常见数字弱密码
-weak_num = ["", "123123", "654321", "1001",
+weak_num = ["",
             "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
             "00", "11", "22", "33", "44", "55", "66", "77", "88", "99", "100",
             "000", "111", "222", "333", "444", "555", "666", "777", "888", "999", "1000",
-            "0000", "1111", "2222", "3333", "4444", "5555", "6666", "7777", "8888", "9999", "10000"
-            "00000", "11111", "22222","33333", "44444", "55555", "66666", "77777", "88888", "99999", "100000",
+            "0000", "1111", "2222", "3333", "4444", "5555", "6666", "7777", "8888", "9999", "10000", "00000", "11111",
+            "22222", "33333", "44444", "55555", "66666", "77777", "88888", "99999", "100000",
             "000000", "111111", "222222", "333333", "444444", "555555", "666666", "777777", "888888", "999999",
-            "12", "123", "1234", "12345", "123456", "12345678", "5211314", "01234", "123123", "456123","654321","9876543210","987654321",
-            "12345678", "123456789"]
+            "12", "123", "1234", "12345", "123456", "12345678", "1001", "5211314", "01234", "123123", "456123",
+            "654321",
+            "9876543210", "987654321",
+            "12345678", "123456789",
+            "112233", "520", "1314"]
+
 # 常见弱口令后缀，如：damin123、admin@123、admin888
-num_suffix = ["123", "888", "111", "12345", "123456"]
+num_suffix = ["123", "1234", "1314", "12345", "123456", "1234567", "123456789",
+              "456789", "88", "888", "8888", "999999", "666", "111", "000"]
+
+# 网址后缀
+url_suffix = ["com", "cn", "org", "com.cn", "edu", "gov", "net"]
+
+# 字符串后缀
+weak_str = ["a", "aa", "aaa", "ab", "abc", "!@#", "!@#456", "qq.com"]
+
 # 常见年份如：qq@2022
-weak_year = ["2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020",
-             "2021", "2022", "2023", "2024"]
-# 常见admin弱口令
-weak_admin = ["admin", "admin123", "admin@123", "admin888", "admin12345", "admin111", "adminadmin", "admintest", "administrator"]
-# 其他常见弱口令相关字符串
-weak_str = ["root", "guest", "root123", "root@123", "user", "ceshi", "ceshi123", "ceshi@123", "test", "test123", "test@123","!@#",
-            "pass", "pass123", "pass@123", "password", "p@ssword", "passw0rd", "NULL", "tomcat", "sys", "system", "qq.com"
-            ]
+weak_year = ["2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019",
+             "2020", "2021", "2022", "2023", "2024", "2025"]
+
+# 常见弱口令
+weak_pass = ["admin", "test", "guest", "root", "pass", "NULL", "druid", "user", "ces", "ceshi", "tomcat", "sys",
+             "system"]
+
 # 强口令
-strong_passwd = ["aa123456", "Aa123456", "Abc123!", "Abc123!@#", "abc123", "abc123!", "abc1234!", "@bcd1234",
-                 "abc123!@#", "Abc123!@#", "#EDC4rfv", "abcABC123", "1qaz!@#$", "admin@123", "Admin@123", "admin@1234",
-                 "Admin@1234", "QAZwsx123", "Pa$$w0rd", "P@ssw0rd", "P@$$word", "P@$$word123", "Abcd1234", "!QAZ2wsx",
-                 "qwe123", "!QAZ3edc", "2wsx#EDC", "123qaz", "1!qaz2@wsx", "1q2w3e4r", "1234abcd", "1234qwer", "1qaz!QAZ", "1qaz2wsx",
-                 "1qaz@WSX", "1qaz@WSX#EDC", "qweasd", "!q2w3e4r", "1234qwer", "1234QWER", "QWER!@#$", "Passwd@123", "Passwd12",
-                 "Passwd@123456", "P@ssw0rd", "P@ssw0rd!", "!QAZ@WSX#EDC", "p@ssw0rd", "P2ssw0rd", "1qaz2wsx3edc",
-                 "abcd123456789", "123456Aa", "Abcd1234", "1234@Qaz#123"]
+strong_passwd = ["admin123", "admin@123", "admin.123", "admin#123", "admin@123", "admin888", "admin12345", "admin111",
+                 "adminadmin", "admintest", "administrator", "Admin@123", "admin@1234", "Admin@1234",
+
+                 "root123", "root@123", "ceshi123", "ceshi@123", "test123", "test@123", "Test@123",
+
+                 "1qaz!@#$", "123qaz", "!QAZ2wsx", "1qaz!QAZ", "1qaz2wsx", "1qaz@WSX", "1!qaz2@wsx", "QAZwsx123",
+                 "!QAZ3edc", "1qaz@WSX#EDC", "!QAZ@WSX#EDC", "1qaz2wsx3edc", "1234@Qaz#123",
+                 "1q2w3e4r", "1234qwer", "!q2w3e4r", "2wsx#EDC", "2wsx@WSX", "QWER!@#$", "#EDC4rfv",
+                 "qweasd", "qwert123", "qwert123", "qwert123", "qwe123", "qwe123456",
+
+                 "12345qwe", "1234QWER", "1234abcd", "123456ab", "abc!@#12",
+
+                 "pass123", "pass@123", "password", "p@ssword", "passw0rd",
+                 "Pa$$w0rd", "P@ssw0rd", "P@$$word", "P@$$word123", "Passwd@123", "Passwd12",
+                 "Passwd@123456", "P@ssw0rd", "P@ssw0rd!", "P2ssw0rd",
+
+                 "aa123456", "AAss1122", "Aa123456", "123456Aa", "aaaAAA123", "a1b2c3d4",
+                 "Abc123!", "Abc123!@#", "abc123", "abc123!", "abc1234!", "@bcd1234", "abc123!@#", "abcd1234",
+                 "Abcd1234",
+                 "abcABC123", "abcd123456789", "Abcd1234", "123123ABC"]
+
+default_dict = weak_num + num_suffix + weak_year + weak_str + weak_pass + strong_passwd
+dict1 = weak_num + num_suffix + weak_year + weak_str
+dict2 = num_suffix + weak_year + weak_str
 
 
-default_dict = weak_num + weak_year + weak_admin + weak_str + strong_passwd
-edit_dict1 = num_suffix + weak_year + weak_admin
-edit_dict2 = weak_num + weak_year + weak_admin
-
-
-# 默认161个字典
 def weak():
     return default_dict
 
@@ -59,53 +82,87 @@ def regular(keyword):
     dicts = []
     dicts.append(keyword)
     dicts = dicts + default_dict
-    dicts = dicts + [keyword + i for i in edit_dict1]
-    dicts = dicts + [i + keyword for i in edit_dict1]
-    dicts = dicts + [keyword.capitalize() + i for i in edit_dict1]
-    dicts = dicts + [i.capitalize() + keyword for i in edit_dict1]
-    dicts = dicts + [keyword + "@" + i for i in edit_dict1]
-    dicts = dicts + [i + "@" + keyword for i in edit_dict1]
-    dicts = dicts + [keyword.capitalize() + "@" + i for i in edit_dict1]
-    dicts = dicts + [i.capitalize() + "@" + keyword for i in edit_dict1]
-    dicts = dicts + [keyword.upper() + "@" + i for i in edit_dict1]
-    dicts = dicts + [keyword.upper() + "@" + i.upper() for i in edit_dict1]
-    dicts = dicts + [keyword + "." + i for i in edit_dict1]
-    dicts = dicts + [keyword + "#" + i for i in edit_dict1]
-    dicts = dicts + [keyword.capitalize() + i + "." for i in edit_dict1]
-    dicts = dicts + [keyword.capitalize() + i + "#" for i in edit_dict1]
+    for i in weak_pass:
+        for j in num_suffix:
+            dicts.append(str(i) + str(j))
+    dicts = dicts + [keyword + i for i in dict1]
+    dicts = dicts + [i + keyword for i in dict1]
+    dicts = dicts + [keyword.capitalize() + i for i in dict1]
+    dicts = dicts + [i.capitalize() + keyword for i in dict1]
+    dicts = dicts + [keyword + "@" + i for i in dict1]
+    dicts = dicts + [i + "@" + keyword for i in dict1]
+    dicts = dicts + [keyword.capitalize() + "@" + i for i in dict1]
+    dicts = dicts + [i.capitalize() + "@" + keyword for i in dict1]
+    dicts = dicts + [keyword.upper() + "@" + i for i in dict1]
+    dicts = dicts + [keyword.upper() + "@" + i.upper() for i in dict1]
+    dicts = dicts + [keyword + "." + i for i in dict1]
+    dicts = dicts + [keyword + "#" + i for i in dict1]
+    dicts = dicts + [keyword.capitalize() + i + "." for i in dict1]
+    dicts = dicts + [keyword.capitalize() + i + "#" for i in dict1]
     return dicts
 
 
-# 全面模式将生成515条字典
-def overall(keyword):
-    dicts = []
-    dicts = dicts + default_dict
-    dicts = dicts + [keyword + i for i in edit_dict2]
-    dicts = dicts + [i + keyword for i in edit_dict2]
-    dicts = dicts + [keyword.capitalize() + i for i in edit_dict2]
-    dicts = dicts + [i.capitalize() + keyword for i in edit_dict2]
-    dicts = dicts + [keyword.upper() + i for i in edit_dict2]
-    dicts = dicts + [i.upper() + keyword for i in edit_dict2]
-    dicts = dicts + [keyword.upper() + i.upper() for i in edit_dict2]
-    dicts = dicts + [keyword + "@" + i for i in edit_dict2]
-    dicts = dicts + [keyword + "@" + i + "." for i in edit_dict2]
-    dicts = dicts + [i + "@" + keyword for i in edit_dict2]
-    dicts = dicts + [keyword.capitalize() + "@" + i for i in edit_dict2]
-    dicts = dicts + [keyword.capitalize() + "@" + i + "." for i in edit_dict2]
-    dicts = dicts + [i.capitalize() + "@" + keyword for i in edit_dict2]
-    dicts = dicts + [keyword.upper() + "@" + i for i in edit_dict2]
-    dicts = dicts + [i.upper() + "@" + keyword for i in edit_dict2]
-    dicts = dicts + [keyword.capitalize() + "@" + i for i in weak_year]
-    dicts = dicts + [keyword.upper() + "@" + i.upper() for i in edit_dict2]
-    dicts = dicts + [keyword + "." + i for i in edit_dict2]
-    dicts = dicts + [keyword + "#" + i for i in edit_dict2]
-    dicts = dicts + [keyword.capitalize() + i + "." for i in edit_dict2]
-    dicts = dicts + [keyword.capitalize() + i + "#" for i in edit_dict2]
-    dicts.append(keyword)
-    dicts.append(keyword+".")
-    dicts.append(keyword + "..")
-    dicts.append(keyword + "...")
-    return dicts
+def processRule(m, name):
+    result = m.tokenize(name)
+    if len(result[0]) > 0 and len(result[1]) == 0:
+        name_list = [''.join([word for word in result[0]]),
+                     result[0][0].capitalize() + ''.join([word for word in result[0][1:]]),
+                     result[0][0].upper() + ''.join([word for word in result[0][1:]]),
+                     ''.join([word.upper() for word in result[0]]), ''.join([word[0] for word in result[0]]),
+                     result[0][0] + ''.join([word[0] for word in result[0][1:]]),
+                     result[0][0][0].upper() + ''.join([word[0] for word in result[0][1:]]),
+                     ''.join([word[0].upper() for word in result[0]])]
+        return name_list
+
+    elif len(result[0]) > 0 and len(result[1]) > 0:
+        name_list = [result[0][0] + ''.join([word for word in result[1]]),
+                     result[0][0].capitalize() + ''.join([word for word in result[1]]),
+                     result[0][0].upper() + ''.join([word for word in result[1]]),
+                     result[0][0].upper() + ''.join([word.upper() for word in result[1]]),
+                     result[0][0][0] + ''.join([word for word in result[1]]),
+                     result[0][0][0].upper() + ''.join([word for word in result[1]]),
+                     result[0][0][0].upper() + ''.join([word.upper() for word in result[1]])]
+        return name_list
+    elif len(result[0]) == 0 and len(result[1]) > 0:
+        name_list = [''.join([word for word in result[1]]),
+                     result[1][0].capitalize() + ''.join([word for word in result[1][1:]]),
+                     ''.join([word.upper() for word in result[1]])]
+        return name_list
+
+
+# 用户名模式生成
+def single_user(username):
+    passwords = []
+    m = PinyinTokenizer()
+    names = processRule(m, username)
+    for i in names:
+        for j in num_suffix:
+            passwords.append(str(i) + str(j))
+    for i in default_dict:
+        passwords.append(i)
+    return passwords
+
+
+# 用户名模式生成
+def list_user(filename, mod):
+    users = []
+    passwords = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        userlist = [i.strip() for i in f.readlines()]
+    for rawname in userlist:
+        m = PinyinTokenizer()
+        names = processRule(m, rawname)
+        password = set()
+        for i in names:
+            for j in num_suffix:
+                password.add(str(i) + str(j))
+        if mod == 2:
+            for i in default_dict:
+                password.add(i)
+        passwords.extend(list(password))
+        for i in range(len(list(password))):
+            users.append(rawname)
+    return users, passwords
 
 
 def take_out_repeat(dict):
@@ -119,14 +176,16 @@ def take_out_repeat(dict):
 if __name__ == '__main__':
     path = os.getcwd()
     print("""
-    请选择字典模式：
+请选择字典模式：
 
-        【1】 默认模式(176条):不结合关键词，直接生成弱口令字典
-        【2】 常规模式(556条):结合关键词进行字典生成，字典较为精简高效如:test@2018
-        【3】 全面模式(1701条):结合关键词进行字典生成，支持复杂得规则如Test@123.
+        【1】 默认模式:不结合关键词，直接生成弱口令字典
+        【2】 常规模式:结合关键词进行字典生成，字典较为精简高效如:test@2018
+        【3】 单用户名模式:根据输入的用户名生成对应的密码字典
+        【4】 多用户名模式:根据输入的用户名列表生成对应的用户名、密码字典
     """)
 
-    model = int(input("请选择模式？ 1 2 3: "))
+    model = int(input("请选择模式？ 1 2 3 4:"))
+    print("\n")
     if model == 1:
         dicts = weak()
         dicts = take_out_repeat(dicts)
@@ -146,9 +205,9 @@ if __name__ == '__main__':
             for i in dicts:
                 f.write(i + '\n')
         f.close()
-    else:
-        keyword = input("请输入目标关键词: ")
-        dicts = overall(keyword)
+    elif model == 3:
+        keyword = input("[*] 请输入目标关键词: ")
+        dicts = single_user(keyword)
         dicts = take_out_repeat(dicts)
         dicts = sorted(dicts, key=lambda i: len(i), reverse=False)
         print(len(dicts))
@@ -156,7 +215,25 @@ if __name__ == '__main__':
             for i in dicts:
                 f.write(i + '\n')
         f.close()
-    print("共生成{}条密码！！请查看！".format(len(dicts)))
+    elif model == 4:
+        filename = input("[*] 请输入存放用户名列表文件地址：")
+        print("""
+    请选择生成模式：
 
+        【1】 默认模式:根据人名生成常见弱口令，如：zhangwei123,zw123456
+        【2】 全面模式:不仅包含根据人名生成的弱口令，也包含常见的弱口令如：Aa123456
 
-
+        """)
+        mod = int(input("请选择模式？ 1 2:"))
+        print("\n")
+        users, passwords = list_user(filename, mod)
+        with open(path + "\\process_usernames.txt", 'w') as f:
+            for i in users:
+                f.write(i + '\n')
+        f.close()
+        with open(path + "\\process_passwords.txt", 'w') as fp:
+            for i in passwords:
+                fp.write(i + '\n')
+        fp.close()
+    print(f"[+] 成功为{len(set(users))}个用户名生成共计{len(passwords)}条密码！！请查看！")
+#
